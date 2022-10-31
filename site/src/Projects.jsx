9bafe2c162bf9c5f651969b98830a9f7543fd518
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-31 17:31:59 +0000

    basic project page laid out, can now add more projects easily

--- site/src/Projects.jsx
+++ site/src/Projects.jsx
@@ -3,21 +3,24 @@ export function ProjectsPage(props) {
     const projects = [
         {
             name: 'My First Website',
-            iframe: <iframe src="https://tkcthulhu.github.io/CSS-HTML-Project/" frameborder="1" height="600" width="900" className="project"/>,
-            description: 'My very first website, this was actually supposed to be an MDN tutorial page but I misunderstood the project like a champion and built this thing from scratch. I don\'t think I\'m going to update these first few projects, to me it\'s always going to be important to remember where I started.',
-            dateCompleted: ''
+            iframe: <iframe src="https://tkcthulhu.github.io/CSS-HTML-Project/" frameborder="1" height="600" width="900" className="project proj-preview"/>,
+            dateCompleted: '09/02/2022',
+            description: 'My very first website, this was actually supposed to be an MDN tutorial page but I misunderstood the project like a champion and built this thing from scratch. Many hours were spent here trying to figure out how to create buttons that actually do things, as well as tying those buttons to things to actually do. In retrospect I would definetly add programming to this list of thing, but it\'s not a hobby, it\'s a career now so I guess it doesn\'t quite meet the criteria. Though this is something that isn\'t pretty, and there was a lot of growing pains in this project, and if I were to redo it now I know it would be 10x better, I don\'t think I\'m going to update this or these other first few projects, to me it\'s always going to be important to remember where I started.',
+            link: 'https://github.com/tkcthulhu/CSS-HTML-Project',
         },
         {
             name: 'The Mind Reader',
-            iframe:  <iframe src="https://tkcthulhu.github.io/mindreader/" frameborder="0" height="844" width="390" className="project"/>,
-            description: '',
-            dateCompleted: '',
+            iframe:  <iframe src="https://tkcthulhu.github.io/mindreader/" frameborder="0" height="844" width="390" className="project proj-preview"/>,
+            dateCompleted: '09/23/2022',
+            description: 'Our first introduction to state in the bootcamp, this was definetly a turning point where the concept of programming finally clicked for me. It was understanding the concept that as a programmer the language is a tool in itself but that the big "AHA" moment is you can literally create your own tools in whatever way you see fit. If something is frustrating or repetitive, make it a function, then find another similar task a repeat, this way you can constantly refine your programs and recycle functions you\'ve created into other projects. Programming truly gives you the ability to make your work as hard or as easy as you want, annoying tasks can be made into functions or automated, your IDE can be set up any way you want, you can work from anywhere with a decent internet connection, it puts the world at your fingertips',
+            link: 'https://github.com/tkcthulhu/mindreader',
         },
         {
             name: 'Thicc Tac Toe',
-            iframe: <iframe src="https://tkcthulhu.github.io/thicc-tac-toe/" frameborder="1" height="1200" width="800" className="project"/>,
-            description: '',
-            dateCompleted: '',
+            iframe: <iframe src="https://tkcthulhu.github.io/thicc-tac-toe/" frameborder="1" height="1200" width="800" className="project proj-preview"/>,
+            dateCompleted: '10/07/2022',
+            description: `The first truly heavy logic lift in our class, this was a challenge that I found incredibly satisfying every obsticle that I was able to overcome. I started this project out by trying first to make it possible to play the game completely in the console with no visual aids, which I did by creating the game board as an array, and creating a function that would loop through and log the indexes for "X" and "O", after that it would run through several possibilities for the win conditions availible on the board. After that I tackled the front end and making it look like a game that I would actually enjoy playing, I used screen record with Notability on my ipad to create the drawing effect of the different marks on the board, I thought it added a nice casual style touch to the game. Lastly I decided to take my first attempt at an AI, which, while tic-tac-toe is to us a simple game, this drove the point home to me of how methodical you must be when explaining a task to a computer, how to tell it what to look for, and in what EXACT order it needs to look for those things. While this is still a very simple AI, with only 7 weeks of programming under my belt at the time I was and still am very proud of it. **SIDE NOTE (If you want to play the AI you have to click the AI button in the bottom right hand corner to trigger an AI move)`,
+            link: 'https://github.com/tkcthulhu/thicc-tac-toe',
         },
     ]
 
@@ -26,13 +29,25 @@ export function ProjectsPage(props) {
     function projCreator(projects) {
         for (let i = 0; i < projects.length; i++) {
             projElements.push(
-                <div className="row">
+                <div className="row proj-row">
                     <div className="col-6 d-flex justify-content-center">
                         {projects[i].iframe}
                     </div>
                     <div className="col-6 d-flex justify-content-center">
-                        <h3>{projects[i].name}</h3><br/>
-                        <p>{projects[i].description}</p>
+                        <div className="container">
+                            <div className="row-12">
+                                <h3 className="text-white proj-title">{projects[i].name}</h3>
+                            </div>
+                            <div className="row-12">
+                                <h4 className="text-white proj-date">{projects[i].dateCompleted}</h4>
+                            </div>
+                            <div className="row-12">
+                                <p className="text-white proj-desc">{projects[i].description}</p>
+                            </div>
+                            <div className="row-12">
+                            <a class="btn btn-light" href={projects[i].link} role="button">Click here to see the repo!</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             )
@@ -42,7 +57,6 @@ export function ProjectsPage(props) {
     return(
         <>
             {projCreator(projects)}
-            {projElements}
         </>
     )
 }
