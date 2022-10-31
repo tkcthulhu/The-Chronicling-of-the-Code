fad39fb8b5c4f7352d77c8242ff6acc1f71acb0f
Tyler Consalvi <tyler.consalvi@student.awesomeinc.org>
2022-10-31 21:37:56 +0000

    styling changes

--- site/src/About-Me.jsx
+++ site/src/About-Me.jsx
@@ -3,11 +3,14 @@ import coffeeComputer from './img/background.jpeg'
 export function AboutMe(props) {
     return(
         <div className="container">
-            <div className="row">
+            <div className="row about-me-row">
                 <div className="col-6">
-                    <img src={coffeeComputer} className='ratio ratio-1x1' />
+                    <img src={coffeeComputer} className='ratio ratio-1x1 about-me-pic' />
                 </div>
                 <div className="col-6">
+                    <h4 className='about-me-title text-white'>The Chronicling of the Consalvi</h4>
+                    <br/>
+                    <br/>
                     <p className='text-white'>As someone who has always had an interest in ripping things apart and seeing how they work, programming has always been something that intereseted me. When the opportunity for getting a start in this field via the Awesome Inc. Web Developer bootcamp was offered to me I knew that I had to take it, there my love for logic problems and puzzles finally met its match in the world of programming. We were challenged every week with a task that was just barely out of our reach, from Tic-Tac-Toe to To-Do Lists, from Weather Apps to D&D, and every week the amount that I learned was multiplied exponentially and my understanding of the craft grew fast and faster. Throughout the bootcamp I have touched a multitude of industry tools and become proficient in them, as well as refining my ability to learn and comprehend new material at a rapid rate. My time here has built my confidence in my abilities and I am ready to hit the ground running in this incredible profession.</p>
                 </div>
             </div>
--- site/src/Home.jsx
+++ site/src/Home.jsx
@@ -4,9 +4,9 @@ import NameLogo from './img/logoName.png'
 export function Home(props) {
     return (
         <div className="container fluid">
-            <div className="row-12">
-                <div className="col d-flex justify-content-center">
-                    <img src={HomePic} alt="Very intelligent looking computer" />
+            <div className="row-12 title-row">
+                <div className="col d-flex justify-content-center" >
+                    <img src={HomePic} alt="Very intelligent looking computer" id='home-pic'/>
                     <img src={NameLogo} alt="Very Handsome Sounding Guy" className='overlay fadeIn' />
                 </div>
             </div>
